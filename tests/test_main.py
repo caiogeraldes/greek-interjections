ccb010522ce8fc333dc1bbd3e0dc5411ed1b4cfb
Caio Geraldes <caio.geraldes@usp.br>
2022-04-12 00:02:32 -0300

    implement main testing

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,6 +2,10 @@ import os
 import pytest
 from greek_interjections.main import main
 
+FIXTURE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data/")
 
-def test_main():
+@pytest.mark.datafiles(os.path.join(FIXTURE_DIR))
+def test_main(datafiles):
+    os.environ["DIORISIS_PATH"] = os.path.join(datafiles, "json/")
+    os.environ["DATA_PATH"] = os.path.join(datafiles)
     main()
